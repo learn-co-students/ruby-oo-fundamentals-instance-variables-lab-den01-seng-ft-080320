0c4aab5cd06d49d1bdd201c372123972406bc7fc
sanjeevyg <yogi.sjv@gmail.com>
2020-08-07 19:32:01 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Dog 
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        @this_dogs_name
+    end
+end
+
+
+Lassie = Dog.new 
+Lassie.name = "Lassie"
+p Lassie.name 
+
+
+ 
+
